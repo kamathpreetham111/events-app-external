ec18159bbe59443ed40b90f7db365647008d265d
kamathpreetham111 <kamath.preetham2@gmail.com>
2024-10-04 14:36:51 +0000

    updated test case

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to Cloud Charlie's application! we are cooking well </h1>"));
         return done();
       });
 
